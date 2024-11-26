2090e47032958d3d32a8181a22732ab89b6ac36a
Gavin Morris <gmorriscs@gmail.com>
2024-11-26 16:23:56 -0600

    Update 2.0

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,21 +1,33 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
 import "./Card.css";
+import { CompanySearch } from '../../company';
+import AddPortfolio from '../Portfolio/AddPortfolio/AddPortfolio';
 
 interface Props {
-companyName: string;
-ticker: string;
-price: number;
+  id: string;
+  searchResult: CompanySearch;
+  onPortfolioCreate: (e: SyntheticEvent) => void;
 }
 
-const Card: React.FC<Props> = ({ companyName, ticker, price }: Props) : JSX.Element  => {
+const Card: React.FC<Props> = ({ id, searchResult, onPortfolioCreate }: Props) : JSX.Element  => {
   return (
-    <div className="card">
-        <div className="details">
-            <h2>{companyName} ({ticker})</h2>
-            <p>USD: ${price}</p>
-        </div>
-        <p className="info">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, enim veritatis cumque, praesentium quos ipsam esse, libero laboriosam distinctio consequuntur modi debitis id? Odit nisi quis illum, maxime delectus quas.</p>
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-black md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-black">{searchResult.currency}</p>
+      <p className="font-bold text-black">
+        {searchResult.exchangeShortName} - {searchResult.stockExchange}
+      </p>
+      <AddPortfolio
+        onPortfolioCreate={onPortfolioCreate}
+        symbol={searchResult.symbol}
+      />
     </div>
   )
 }
